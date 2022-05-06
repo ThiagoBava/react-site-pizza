6dcf0365bdb91f57b784ce36927903c2253f4eca
Thiago Bavaresco <thiagobavaresco@unochapeco.edu.br>
2022-05-06 11:13:36 -0300

    master

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Nav, NavLink} from './NavbarElements';
+
+const Navbar = () => {
+  return (
+    <>
+        <Nav>
+            <NavLink to='/'>SnackUno</NavLink>
+        </Nav>
+    </>
+  )
+}
+
+export default Navbar;
