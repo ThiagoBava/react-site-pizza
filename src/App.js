8eee97ae2f23130a58cb54148bd1e909ee5252a7
Thiago Bavaresco <thiagobavaresco@unochapeco.edu.br>
2022-05-06 16:44:19 -0300

    'update navbar'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './components/NavBar';
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { GlobalStyle } from './globalStyles';
 
 function App() {
   return (
     <Router>
+      <GlobalStyle />
       <Navbar />
     </Router>
   );
--- src/components/NavBar/NavbarElements.js
+++ src/components/NavBar/NavbarElements.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
 import { NavLink as Link } from 'react-router-dom';
+import { FaPizzaSlice } from 'react-icons/fa';
 
 export const Nav = styled.nav`
-  background: transparent;
+  background: #8C0400;
   height: 80px;
   display: flex;
   justify-content: center;
@@ -23,3 +24,21 @@ export const NavLink = styled(Link)`
     left: 25px;
   }
 `;
+export const NavIcon = styled.div`
+display: block;
+position: absolute;
+top: 0;
+left: 0;
+cursosr: pointer;
+color: #fff;
+
+p{
+  transform: translate(175%, 100%);
+  font-weight: bold;
+}
+`
+
+export const Bars = styled(FaPizzaSlice)`
+font-size: 2rem;
+transform: translate(50%, 15%);
+`
