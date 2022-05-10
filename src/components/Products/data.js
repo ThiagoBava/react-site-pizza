9a449ce754e9059d0b2ce05c6b16a995b22bb7b4
Thiago Bavaresco <thiagobavaresco@unochapeco.edu.br>
2022-05-10 17:01:28 -0300

    'Produtos criados'

---
+++ src/components/Products/data.js
@@ -0,0 +1,30 @@
+import product1 from '../../images/product-1.jpg'
+import product2 from '../../images/product-2.jpg'
+import product3 from '../../images/product-3.jpg'
+
+export const productData = [
+    {
+        img: product1,
+        alt: 'Pizza',
+        name: 'Mini Pizza',
+        desc: 'Calabresa, Quatro Queijos, Frango',
+        price: 'R$ 05,00',
+        button: 'Adcione a Cesta'
+    },
+    {
+        img: product2,
+        alt: 'Hamburguer',
+        name: 'Hamburgão',
+        desc: 'Pão, hamburguer, alface, queijo, milho, ervilha, maionese',
+        price: 'R$ 10,00',
+        button: 'Adcione a Cesta'
+    },
+    {
+        img: product3,
+        alt: 'Pastel',
+        name: 'Pastel',
+        desc: 'Carne, Frango, Queijo',
+        price: 'R$ 06,00',
+        button: 'Adcione a Cesta'
+    }
+]
