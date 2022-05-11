14712daaa09b0b6f123d6b61e6f3c479fcefe303
Thiago Bavaresco <thiagobavaresco@unochapeco.edu.br>
2022-05-11 19:53:15 -0300

    'Feature criado'

---
+++ src/components/Feature/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {FeatureContainer, FeatureButton} from './FutureElements';
+
+const Feature = () => {
+  return (
+    <FeatureContainer>
+        <h1>Promoção do Dia</h1>
+        <p>O lanche mais comprado do mês, Mini Pizza Calabresa</p>
+        <FeatureButton>Compre Já</FeatureButton>
+    </FeatureContainer>
+  )
+}
+
+export default Feature
