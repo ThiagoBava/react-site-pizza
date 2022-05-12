f4aa69590fb90a3720ac45645ae7145b17ff45de
Thiago Bavaresco <thiagobavaresco@unochapeco.edu.br>
2022-05-12 13:29:26 -0300

    'Footer Criado'

--- src/components/Products/data.js
+++ src/components/Products/data.js
@@ -1,6 +1,9 @@
 import product1 from '../../images/product-1.jpg'
 import product2 from '../../images/product-2.jpg'
 import product3 from '../../images/product-3.jpg'
+import sweet1 from '../../images/sweet.jpg'
+import sweet2 from '../../images/sweet2.jpg'
+import sweet3 from '../../images/sweet3.jpg'
 
 export const productData = [
     {
@@ -21,6 +24,33 @@ export const productData = [
     },
     {
         img: product3,
+        alt: 'Pastel',
+        name: 'Pastel',
+        desc: 'Carne de gado, Frango, Mortadela e queijo',
+        price: 'R$ 06,00',
+        button: 'Adcione a Cesta'
+    }
+];
+
+export const productDataTwo = [
+    {
+        img: sweet1,
+        alt: 'Pizza',
+        name: 'Mini Pizza',
+        desc: 'Calabresa, Quatro Queijos, Frango',
+        price: 'R$ 05,00',
+        button: 'Adcione a Cesta'
+    },
+    {
+        img: sweet2,
+        alt: 'Hamburguer',
+        name: 'Hamburgão',
+        desc: 'Pão, hamburguer, alface, queijo, milho, ervilha, maionese',
+        price: 'R$ 10,00',
+        button: 'Adcione a Cesta'
+    },
+    {
+        img: sweet3,
         alt: 'Pastel',
         name: 'Pastel',
         desc: 'Carne de gado, Frango, Mortadela e queijo',
